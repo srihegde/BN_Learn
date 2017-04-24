d0d5b446d1e70f5da1da0e519e91f017d726e677
Srinidhi Hegde <shrinidhihegde@gmail.com>
2017-04-24 20:16:19 +0530

    Added working Simmulated Annealing

--- bayesnet.py
+++ bayesnet.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pydot
+from random import randint, random
 import time
 import datetime
 
@@ -10,11 +11,12 @@ class BayesNet():
 		self.num_nodes = num_nodes
 		self.grph = np.zeros((num_nodes, num_nodes))
 		self.num_edges = 0
+		self.BIC = 0
 
 	def addEdge(self, a, b):
 		assert ((a >= 0 or a < self.num_nodes) or (b >= 0 or b < self.num_nodes)), 'Vertex index out of bounds!'
 		self.grph[a][b] = 1
-		if self.isDAG() == False or a == b:
+		if self.isCyclic() == True or a == b:
 			self.grph[a][b] = 0
 			# print 'Could not add edge because it\'s no longer a DAG.'
 			return False
@@ -49,28 +51,34 @@ class BayesNet():
 		return False
 
 
-	def isDAG(self):
-		path = set()
-		visited = set()
+	def isCyclic(self):
+		visited = [False for i in xrange(self.num_nodes)]
+		recstack = [False for i in xrange(self.num_nodes)]
 
-		def visit(vertex):
-			if vertex in visited:
-				return False
-			visited.add(vertex)
-			path.add(vertex)
-			for i in xrange(self.num_nodes):
-				if self.grph[vertex][i] == 1:
-					if (i in path and i != vertex) or visit(i):
-						return False
-			path.remove(vertex)
-			return True
+		def isCyclicUtil(v):
+			if not visited[v]:
+				visited[v] = True
+				recstack[v] = True
 
-		return any(visit(v) for v in xrange(self.num_nodes))
+				for i in xrange(self.num_nodes):
+					if self.grph[v][i] == 1:
+						if not visited[i] and isCyclicUtil(i):
+							return True
+						elif recstack[i]:
+							return True
+
+			recstack[v] = False
+			return False
+
+		return any(isCyclicUtil(v) for v in xrange(self.num_nodes))
 		
 
 	def showNet(self, fname = 'bn_'+'{:%Y-%m-%d_%H:%M:%S}'.format(datetime.datetime.now())+'.png'):
 		graph = pydot.Dot(graph_type='digraph')
 		
+		for i in xrange(self.num_nodes):
+			graph.add_node(pydot.Node(str(i+1)))
+
 		for i in xrange(self.num_nodes):
 			for j in xrange(self.num_nodes):
 				if self.grph[i][j] == 1:
@@ -82,19 +90,20 @@ class BayesNet():
 		time.sleep(2)
 
 
+	# Computing BIC score for a given Bayesian Network
+	def getBIC(self):
+		# Temp scoring. Need to change
+		nodes = self.num_nodes
+		prior = 2*self.num_edges/float(nodes*(nodes-1))
+		lhood = random()
+		self.BIC = lhood*prior
+		return self.BIC
 
-'''if __name__ == '__main__':
+
+
+if __name__ == '__main__':
 	
 	bnet = BayesNet(3)
 	bnet.addEdge(0,1)
-	bnet.addEdge(1,2)
-	# bnet.showNet()
-
-	bnet.addEdge(2,0)
-	# bnet.showNet()
-	
-	bnet.addEdge(0,2)
+	bnet.addEdge(1,0)
 	bnet.showNet()
-
-	bnet.reverseEdge(0,2)
-	bnet.showNet() '''
--- bn_learn.py
+++ bn_learn.py
@@ -1,7 +1,9 @@
 import numpy as np
-import bayesnet as bn
 import math
 from random import randint, random
+import xlrd as xl
+
+import bayesnet as bn
 
 
 # Randomly initialize the Bayesian Network
@@ -11,37 +13,48 @@ def randInitBN(bnet):
 		while not bnet.addEdge(randint(0,nodes-1), randint(0,nodes-1)): pass
 	return bnet
 
-# Computing BIC score for a given Bayesian Network
-def computeBIC(bnet):
-	# Temp scoring. Need to change
-	return -2*bnet.num_edges
-
 # Randomly pick next element of search space
 def pickNextBN(bnet):
 	opt = randint(0,2)
+	cnt,attempts = 0,20
 	if opt == 0:
-		while not bnet.addEdge(randint(0,bnet.num_nodes), randint(0,bnet.num_nodes)): pass
+		while not bnet.addEdge(randint(0,bnet.num_nodes-1), randint(0,bnet.num_nodes-1)) and cnt < attempts:
+			cnt += 1
+		if cnt == attempts:
+			return pickNextBN(bnet)
 	elif opt == 1:
-		while not bnet.reverseEdge(randint(0,bnet.num_nodes), randint(0,bnet.num_nodes)): pass
+		while not bnet.reverseEdge(randint(0,bnet.num_nodes-1), randint(0,bnet.num_nodes-1)) and cnt < attempts:
+			cnt += 1
+		if cnt == attempts:
+			return pickNextBN(bnet)
 	else:
-		while not bnet.deleteEdge(randint(0,bnet.num_nodes), randint(0,bnet.num_nodes)): pass
+		while not bnet.deleteEdge(randint(0,bnet.num_nodes-1), randint(0,bnet.num_nodes-1)) and cnt < attempts:
+			cnt += 1
+		if cnt == attempts:
+			return pickNextBN(bnet)
 	return bnet
 
 
 # Search the search space using Simulated Annealing
-def searchSimAnn(bnet, conv_delta):
+def searchSimAnn(bnet, conv_delta, temp = 1000, delta = 1):
 	best_bn = bnet
-	temp, delta = 1000,1
 	while temp > 0:
-		t -= delta
+
 		new_bn = pickNextBN(best_bn)
-		del_score = computeBIC(new_bn) - computeBIC(best_bn)
+		del_score = new_bn.getBIC() - best_bn.getBIC()
+		
 		if del_score > 0:
 			best_bn = new_bn
 		else:
 			r = random()
 			if r < math.exp(del_score/temp):
 				best_bn = new_bn
+		
+		if temp%10 == 0:
+			print 'Iteration ',1000-temp,':'
+		if(temp%250 == 0):
+			best_bn.showNet('interBN_'+str(1000-temp)+'.png')
+		temp -= delta
 
 	return best_bn
 
@@ -53,5 +66,5 @@ if __name__ == '__main__':
 	bnet = randInitBN(bnet)
 	bnet.showNet('initialBN.png')
 
-	bnet = searchSimAnn(bnet, 1)
+	bnet = searchSimAnn(bnet, 1, 1000)
 	bnet.showNet('finalBN.png')
